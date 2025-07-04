2bdb032995aa2a15b871edfad1425dcab0e3a3d6
Number_Sir <number_sir@126.com>
2025-07-04 23:26:17 +0800

    feat(conversion): change keys for lotr-extended speech

--- src/core.py
+++ src/core.py
@@ -517,7 +517,7 @@ class Conversion:
             for idx, speech in enumerate(original["speech"]):
                 result.extend([
                     Data(
-                        key=f"{'.'.join(filepath.with_suffix('').parts)}.{idx}.{idx_}",
+                        key=f"{'.'.join(filepath.with_suffix('').parts)}.speech{idx}.line{idx_}",
                         original=line,
                         translation="",
                     )
